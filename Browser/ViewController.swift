99e8a243440727500de22a1fcf699ad7ea7af8d9
舟木正憲 <funakimfunaki@gmail.com>
2018-12-29 13:36:01 +0900

    Webサイトを表示する実装

--- Browser/ViewController.swift
+++ Browser/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // URL の文字列をURL型に変換
+        // 変換した結果オプショナル型になる
+        // nil の可能性があり、それを防ぐため、
+        // if を使って、値が nil ではないときに実行するようにし、
+        // if let url = と書くことで、オプショナル型ではなくなり、url 型が入る
+        if let url = URL(string: "http://www.google.com") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+
+        webView.navigationDelegate = self
     }
 
+    @IBAction func reloadTapped(sender: UIButton) {
+        webView.reload()
+    }
 
+    @IBAction func backTapped(sender: UIButton) {
+        webView.goBack()
+    }
+
+    @IBAction func forwardTapped(sender: UIButton) {
+        webView.goForward()
+    }
+
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Start...")
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Finish!")
+    }
 }
 
